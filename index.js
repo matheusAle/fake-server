118938a9698bdc13113145d0da167a0090fb28c0
Matheus Ale <m.matheus.ale@gmail.com>
2023-02-01 11:08:07 -0300

    update procfile

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const middlewares = jsonServer.defaults()
 
 const getAuth = name => (process.env[name] || process.env.AUTHORIZATION);
 
+// Use port from Heroku, fall back to default port
+const port = process.env.PORT || 3000;
+
 const AuhMiddleware = (authorization) => (req, res, next) => {
   if (req.headers['authorization'] === authorization) {
     return next();
@@ -32,6 +35,7 @@ server.use(
 server.use('/feracode/look-after', AuhMiddleware(getAuth('AUTH_FERACODE')), routerLookAfter);
 server.use('/vuttr', AuhMiddleware(getAuth('AUTH_VUTTR')), routerVUTTR);
 server.use('/evolucional/job-test', AuhMiddleware(getAuth('AUTH_EVOLUCIONAL')), evolucionalJoBTest);
-server.listen(process.env.PORT ||3000, () => {
+
+server.listen(port, () => {
   console.log('JSON Server is running')
 });
